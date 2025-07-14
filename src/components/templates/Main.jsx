b06d7a7d384492a43657576f104e8e64ce3013b4
Soudabeh Nourollahi <s.nourollahii@gmail.com>
2025-07-14 22:36:20 +0200

    feat(main): display list of posts with title, city, price, and image

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,5 +1,27 @@
-function Main({posts}) {
-  return <div>Main</div>;
+function Main({ posts }) {
+  const baseURL = import.meta.env.VITE_BASE_URL;
+
+  return (
+    <div>
+      {posts?.data?.posts?.map((post) => (
+        <div key={post._id}>
+          <div>
+            <p>{post.options?.title || "No title"}</p>
+            <div>
+              <span className="font-bold">
+                {post.amount.toLocaleString("de-DE", {
+                  style: "currency",
+                  currency: "EUR",
+                })}
+              </span>
+              <span>{post.options?.city || "Unknown city"}</span>
+            </div>
+          </div>
+          <img src={`${baseURL}${post.images[0]}`}/>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Main;
