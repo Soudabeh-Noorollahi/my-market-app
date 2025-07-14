8bdc93420d6a6f7699d133776d2c836de1a9ee58
Soudabeh Nourollahi <s.nourollahii@gmail.com>
2025-07-14 13:41:40 +0200

    feat: create basic Sidebar component layout

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,5 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "@/services/admin";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const { data } = useQuery(["get-categories"], getCategory);
+  console.log(data);
+
+  return (
+    <div>
+      <h4>Categories</h4>
+      <ul>
+        {data?.data.map((category) => (
+          <li key={category._id}>
+            <img src={`${category.icon}.svg`} />
+            <p>{category.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Sidebar;
